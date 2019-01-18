54a5d6974374504efca21d36b0c3b2b5b81bbc9d
Raj Patel <rajp4690@gmail.com>
2019-01-18 15:06:28 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'vishal',
-        text: 'Hey this is Raj',
-    });
 });
 
 socket.on('disconnect', function () {
